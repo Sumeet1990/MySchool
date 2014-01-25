b23b1f3c7a98bbedefff98991bcafbeb060551dc
ishan086 <ishan086@gmail.com>
2014-01-25 18:21:35 +0530

    added two classes which got missed

---
+++ src/com/myschool/action/ConfigurationMenuAction.java
@@ -0,0 +1,17 @@
+package com.myschool.action;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+/*
+ * 
+ */
+public class ConfigurationMenuAction extends ActionSupport {
+	
+	private static Logger log = Logger.getLogger(ConfigurationMenuAction.class);
+	
+	
+}
---
+++ src/com/myschool/action/FunctionalMenuAction.java
@@ -0,0 +1,14 @@
+package com.myschool.action;
+
+import org.apache.log4j.Logger;
+import com.opensymphony.xwork2.ActionSupport;
+
+
+/*
+ * 
+ */
+public class FunctionalMenuAction extends ActionSupport {
+	
+	private static Logger log = Logger.getLogger(FunctionalMenuAction.class);	
+	
+}
